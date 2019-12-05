4bf992f3745a6611ab02bf35155486d7c2ff21bb
odmishien <tmtm.yagi.007@gmail.com>
2019-12-05 17:50:03 +0900

    skip-lockとかいれる

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ cd $SCRIPT_DIR
 cd ../
 
 # copy resources to ec2
-scp -i ~/.ssh/reco.pem -r ./* ec2-user@18.176.126.121:/home/ec2-user
+rsync -e "ssh -i ~/.ssh/reco.pem" --exclude '.*' -rltvz ./ ec2-user@18.176.126.121:/home/ec2-user
 
 # start server
-ssh -i ~/.ssh/reco.pem ec2-user@18.176.126.121 make run &&
+ssh -i ~/.ssh/reco.pem ec2-user@18.176.126.121 make run-with-build
